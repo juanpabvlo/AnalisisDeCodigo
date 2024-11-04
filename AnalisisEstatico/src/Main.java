12c8646064ce1fc3d4b917dbf9a2eeceda81f222
juanpabvlo <114199889+juanpabvlo@users.noreply.github.com>
2024-11-04 16:54:59 -0500

    Update Main.java

--- AnalisisEstatico/src/Main.java
+++ AnalisisEstatico/src/Main.java
@@ -11,7 +11,6 @@ public class Main {
         for (Libro libro : biblioteca.catalogo) {
             String x;
         }
-
         biblioteca.agregarLibro(libro2);
         biblioteca.agregarLibro(libro3);
 
